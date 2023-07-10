08d7eab80522400e27b3e2f83eb87cf49b105bd6
manishgautammg3994 <manishgautammg3994@gmail.com>
2023-07-10 10:30:24 +0530

    starter

---
+++ lib/views/add_item_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AddItemsPage extends StatefulWidget {
+  @override
+  AddItemsPageState createState() => AddItemsPageState();
+}
+
+class AddItemsPageState extends State<AddItemsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0, // for background transparent
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.grey[900],
+        title: const Text('Add Items'),
+      ),
+      body: Container(
+        height: 100.0,
+      ),
+    );
+  }
+}
